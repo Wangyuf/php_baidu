b338a38705c1404d42137860160b7643223cea32
dsczs <370637594@qq.com>
2016-03-26 17:34:00 +0800

    1

--- Application/Home/View/Login/index.php
+++ Application/Home/View/Login/index.php
@@ -7,7 +7,7 @@
 <body>
     <form action="<?php echo U('Login/index');?>" method="post">
         name:<input type="text" name="username" value='xiaogangshagua'/><br />
-        password:<input type="password" name="password" value='hadoop123'/><br />
+        password:<input type="password" name="password" value=''/><br />
         <input type="submit" value="登录">
     </form>
     <a href='<?php echo U('Login/testLogin');?>'>测试登录是否成功</a>
